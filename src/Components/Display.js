3bc66cc3b573942b40ea711dbad2650b4cfc690e
BasemHasan <bhasan@botkeeper.com>
2022-06-05 17:18:19 +0200

    fix video embed and add video title

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -3,21 +3,29 @@ import React, { Component } from "react";
 export default class Display extends Component {
   render() {
     let items = this.props.items;
-
-    const videos = items.map((item) => {
-      return (
-        <div className="d-inline">
-          <iframe
-            className="border border-danger my-2 mx-2"
-            width="250"
-            height="250"
-            src="http://www.youtube.com/embed/${items.id.videosId}"
-            frameborder="0"
-          ></iframe>
+    return (
+      <div class="container">
+        <div class="row">
+        {
+          items.map((item, index) => {
+            const videoId = item.id.videoId
+            const videoTitle = item.snippet.title
+            return (
+              <div className="col-3 p-4 mt-4" key={index}>
+                  <h4 className="d-inline-block text-truncate" title={videoTitle} style={{maxWidth: '250px'}}>{videoTitle}</h4>
+                  <iframe
+                    className="border border-danger my-2 mx-2"
+                    width="100%"
+                    height="100%"
+                    src={`http://www.youtube.com/embed/${videoId}`}
+                    frameBorder="0"
+                  ></iframe>
+              </div>
+            )
+          })
+        }
         </div>
-      );
-    });
-
-    return <div>{videos}</div>;
+      </div>
+    )
   }
 }
